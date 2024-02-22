f9cf274fd1076810ccc8028955fde8add3516062
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-22 14:02:20 +0100

    Updated the CTA section

--- src/components/Hero/CTA.jsx
+++ src/components/Hero/CTA.jsx
@@ -3,6 +3,14 @@ import React from "react";
 const CTA = () => {
 	return (
 		<div className="cta">
+			<a
+				href="https://docs.google.com/document/d/11YH-aPEKmm3rbQxfOTMnYhHzzS0eyXsIfaNZn2pd3d0/edit?usp=sharing"
+				className="btn btn-primary"
+				target="_blank" rel="noreferrer"
+			>
+				Download CV
+			</a>
+
 			<a href="#contact" className="btn btn-primary">
 				Let's Talk
 			</a>
